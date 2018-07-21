802f4e5516f328d2fe255e1e759a69c719e2064a
William Holmes <holmesw@ie.ibm.com>
2018-07-22 00:40:21 +0100

    undo tooltp

--- src/client/app/components/sections/rows/DomainRow.js
+++ src/client/app/components/sections/rows/DomainRow.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import ToolTip from 'rc-tooltip';
 
-const DomainRow = ({ domain, state, details, time, description }) => (
-  <div className={'row'}>
+const DomainRow = ({ domain, internal, state, details, time }) => (
+  <div className={'row'} data-domain-type={internal ? 'internal': 'external'}>
     <input type={'radio'} name={'expand'}/>
     <span className={'cell primary'} data-label={'Status'}>
       <span className={`service service-${state}`}></span>
-      <span className={'small-detail'}>
-        <ToolTip placement={'right'} trigger={['hover']} overlay={<span>{description}</span>}>{domain}</ToolTip>
-      </span>
+      <span className={'small-detail'}>{domain}</span>
     </span>
-    <span className={'cell'} data-label={'Domain'}>
-      <ToolTip placement={'right'} trigger={['hover']} overlay={<span>{description}</span>}>{domain}</ToolTip>
-      </span>
+    <span className={'cell'} data-label={'Domain'}>{domain}</span>
     <span className={'cell'} data-label={'Details'}>{details}</span>
     <span className={'cell'} data-label={'Time'}>{time}</span>
   </div>
