a2109ba58a407b64dacafcb4198aef2c4de50771
William Holmes <holmesw@ie.ibm.com>
2018-07-21 23:51:55 +0100

    Initial Tooltip

--- src/client/app/components/sections/rows/DomainRow.js
+++ src/client/app/components/sections/rows/DomainRow.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import ToolTip from 'rc-tooltip';
 
-const DomainRow = ({ domain, internal, state, details, time }) => (
-  <div className={'row'} data-domain-type={internal ? 'internal': 'external'}>
+const DomainRow = ({ domain, state, details, time, description }) => (
+  <div className={'row'}>
     <input type={'radio'} name={'expand'}/>
     <span className={'cell primary'} data-label={'Status'}>
       <span className={`service service-${state}`}></span>
-      <span className={'small-detail'}>{domain}</span>
+      <span className={'small-detail'}>
+        <ToolTip placement={'right'} trigger={['hover']} overlay={<span>{description}</span>}>{domain}</ToolTip>
+      </span>
     </span>
-    <span className={'cell'} data-label={'Domain'}>{domain}</span>
+    <span className={'cell'} data-label={'Domain'}>
+      <ToolTip placement={'right'} trigger={['hover']} overlay={<span>{description}</span>}>{domain}</ToolTip>
+      </span>
     <span className={'cell'} data-label={'Details'}>{details}</span>
     <span className={'cell'} data-label={'Time'}>{time}</span>
   </div>
